b8f03e01e89fe1737358d68e9c27f4de3b42e0ce
chedim <>
2022-04-07 09:10:26 -0400

    Adds springdoc openapi configuration to support reverse proxy on gitpod

--- src/main/java/com/couchbase/example/CouchbaseTransactionsDemoApplication.java
+++ src/main/java/com/couchbase/example/CouchbaseTransactionsDemoApplication.java
@@ -2,6 +2,8 @@ package com.couchbase.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 public class CouchbaseTransactionsDemoApplication {
@@ -10,4 +12,8 @@ public class CouchbaseTransactionsDemoApplication {
 		SpringApplication.run(CouchbaseTransactionsDemoApplication.class, args);
 	}
 
+  @Bean
+  ForwardedHeaderFilter forwardedHeaderFilter() {
+     return new ForwardedHeaderFilter();
+  }
 }
